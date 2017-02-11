eb0296bdc4ae78b9ec3a950c41c166842c93c150
Zen Lu <arguingcrab@gmail.com>
2017-02-10 16:59:24 -0800

    Revert "Blogging with Phoenix #3"
    This reverts commit d58236a1828832ccd1f62419e391487f028d4851.

---
+++ pxblog/web/models/post.ex
@@ -0,0 +1,21 @@
+defmodule Pxblog.Post do
+  use Pxblog.Web, :model
+
+  schema "posts" do
+    field :title, :string
+    field :body, :string
+
+    timestamps()
+
+    belongs_to :user, Pxblog.User
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
